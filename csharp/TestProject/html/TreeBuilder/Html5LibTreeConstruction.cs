ff2c27dfacecc8af3971e92746db3bd8c401905e
Lorenz Ackermann <lpas@gmx.de>
2025-07-22 14:25:57 +0200

    prepare tree construction tests for running over more test files

--- csharp/TestProject/html/TreeBuilder/Html5LibTreeConstruction.cs
+++ csharp/TestProject/html/TreeBuilder/Html5LibTreeConstruction.cs
@@ -9,17 +9,27 @@ public sealed class Html5LibTreeConstruction {
 
     private static string ProjectDirectory => Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.Parent.Parent.FullName;
 
+    private static (string, (int[] skipTests, int[] expectWrongTree, int[] expectWrongErrors))[] files = [
+        ("tests1.dat", ([29, 30, 57, 99, // tree building has problems infinity running tests
+            70, 71, 72, 73, 74, 75,   // adoption agency index out of range
+            102], // not implement it),
+            [22, 23, 31, 32, 51, 52, 53, 56, 60, 77, 78, 79, 80, 90, 95, 96, 101, 110],
+            [27, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 55] // tree is ok but parse error miss match
+        ))
+    ];
+
+
     [TestMethod]
     public void ReadFileVsReadStr() {
-        var filePath = Path.Combine(ProjectDirectory, "html5lib-tests", "tree-construction", "tests1.dat");
-        var testReader = TestReader.CreateFromFile(filePath);
-        int[] skipTests = [29, 30, 57, 99, // tree building has problems infinity running tests
-            70, 71, 72, 73, 74, 75,   // adoption agency index out of range
-            102]; // not implement it
-        // todo fix 31 attributes
-        int[] expectWrongTree = [22, 23, 31, 32, 51, 52, 53, 56, 60, 77, 78, 79, 80, 90, 95, 96, 101, 110];
-        int[] expectWrongErrors = [27, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 55]; // tree is ok but parse error miss match
+        foreach (var (file, (skipTests, expectWrongTree, expectWrongErrors)) in files) {
+            var filePath = Path.Combine(ProjectDirectory, "html5lib-tests", "tree-construction", file);
+            Console.WriteLine(file);
+            RunTestsForFile(filePath, skipTests, expectWrongTree, expectWrongErrors);
+        }
+    }
 
+    private static void RunTestsForFile(string filePath, int[] skipTests, int[] expectWrongTree, int[] expectWrongErrors) {
+        var testReader = TestReader.CreateFromFile(filePath);
         foreach (var (testCase, index) in testReader.GetTestCases().Select((testCase, i) => (testCase, i))) {
             Console.WriteLine(index);
             if (skipTests.Contains(index)) continue;
